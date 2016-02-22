15dfa595976729278997f66d2adfb41f0ef9377d
victorkwak <victorkwak@gmail.com>
2016-02-21 16:30:22 -0800

    Init

---
+++ src/Empty.scala
@@ -0,0 +1,11 @@
+/**
+  * Victor Kwak
+  */
+class Empty extends Square {
+  def this(x: Int, y: Int) {
+    this()
+    this.x = x
+    this.y = y
+    symbol = " - "
+  }
+}
---
+++ src/Square.scala
@@ -0,0 +1,21 @@
+/**
+  * Victor Kwak
+  */
+class Square (var x: Int, var y: Int, val symbol: String){
+
+  def setXY(x: Int, y: Int) {
+    this.x = x
+    this.y = y
+  }
+
+  override def equals(o: Any): Boolean = {
+    if (!o.isInstanceOf[Square]) {
+      return false
+    }
+    val compare: Square = o.asInstanceOf[Square]
+
+    symbol == compare.symbol && x == compare.x && y == compare.y
+  }
+
+  override def toString = String.valueOf(symbol)
+}
