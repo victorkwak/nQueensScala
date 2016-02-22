b8b6e3c7372ccc948da2f780719dae30a3bca938
victorkwak <victorkwak@gmail.com>
2016-02-21 16:41:49 -0800

    Queen class

--- src/Empty.scala
+++ src/Empty.scala
@@ -1,11 +1,4 @@
 /**
   * Victor Kwak
   */
-class Empty extends Square {
-  def this(x: Int, y: Int) {
-    this()
-    this.x = x
-    this.y = y
-    symbol = " - "
-  }
-}
+class Empty(x: Int, y: Int) extends Square(x, y, " - ")
---
+++ src/Queen.scala
@@ -0,0 +1,4 @@
+/**
+  * Victor Kwak
+  */
+class Queen(x: Int, y: Int) extends Square(x, y, " Q ")
